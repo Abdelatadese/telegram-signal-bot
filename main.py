22c795c9c47c1b1b3398c496297e0d9539141a0d
Abdelatadese <abdelatadese877@gmail.com>
2026-09-06 20:51:03 +0300

    Fix formatting in main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def home():
     return "Bot is alive and running!"
 
 def run_flask():
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000)) 
     app.run(host="0.0.0.0", port=port)
 
 # Telegram Bot commands
